efeb5a939712fa8d1f8f5822535add1a90bd35fb
metabrez <metabrez@gmail.com>
2018-01-26 20:24:04 -0600

    third commit

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello{
 
 	public static void main(String[] args) {
 		
+		System.out.println("Hello testing");
 		for (int i=1;i<=11 ;i++ ) {
 
 			System.out.println("Hello Worlld" +i);
